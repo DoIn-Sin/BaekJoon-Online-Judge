e5618c51d148b4b6caa3d311aaae4c3070761fa6
DoIn-Sin <sindoin0509@gmail.com>
2022-07-25 23:53:23 +0900

    10814 나이순 정렬 sol

---
+++ 10814_나이순_정렬.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+member_lst = []
+
+for i in range(n):
+    age, name = map(str, input().split())
+    age = int(age)
+    member_lst.append((age, name))
+
+member_lst.sort(key = lambda x : x[0])
+
+for i in member_lst:
+    print(i[0], i[1])
