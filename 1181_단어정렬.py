79b14a0f001a4c2033cc0f5aa71b5e33c0796a2d
DoIn-Sin <sindoin0509@gmail.com>
2022-08-04 23:49:05 +0900

    1181 단어 정렬 sol

---
+++ 1181_단어정렬.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = []
+
+for i in range(n):
+    lst.append(input().strip())
+    
+set_lst = set(lst)
+lst = list(set_lst)
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
