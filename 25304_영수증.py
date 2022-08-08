8013e86bfa3872545888a1980c5b6b0a9dfb4cf6
도인 <77676907+DoIn-Sin@users.noreply.github.com>
2022-08-08 23:34:37 +0900

    25304 영수증 sol

---
+++ 25304_영수증.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+X = int(input())
+N = int(input())
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    X -= a*b
+
+if not X:
+    print('Yes')
+else:
+    print('No')
